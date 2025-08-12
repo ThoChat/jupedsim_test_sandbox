12d7f1c1515c7c254e498b62872c4c3db3a128ca
ThoChat <83350421+ThoChat@users.noreply.github.com>
2025-08-12 17:32:41 +0200

    sim setup based on experiments

---
+++ comparison_with_experiments/sim_experimental_setup.py
@@ -0,0 +1,109 @@
+# 040_l020_g1_rf_h-: POLYGON ((5 -5, 5 2, -5 2, -5 -5, 5 -5), (0.2 0, 3.2 0, 3.2 1, 1 1, 1 0.2, 0.2 0.2, 0.2 0), (-0.2 0, -0.2 0.2, -1 0.2, -1 1, -3.2 1, -3.2 0, -0.2 0))
+
+import pathlib
+import jupedsim as jps
+import pedpy
+from numpy.random import normal  # normal distribution of free movement speed
+from shapely import Polygon
+from shapely.wkt import loads
+from shapely.ops import unary_union
+import matplotlib.pyplot as plt
+
+
+## Setup geometries
+area_exp = loads(
+    "POLYGON ((5 -5, 5 2, -5 2, -5 -5, 5 -5), "
+    "(0.2 0, 4.99 0, 4.99 1, 1 1, 1 0.2, 0.2 0.2, 0.2 0), "
+    "(-0.2 0, -0.2 0.2, -1 0.2, -1 1, -4.99 1, -4.99 0, -0.2 0)))"
+)
+
+
+### to visualize trajectories ###
+# walkable_area = pedpy.WalkableArea(area_exp)
+# file = pathlib.Path(
+#     "./../../../Juelich_database/bottleneck_single_individuals/trajectories_hdf5/040_l020_g1_rf_h-.h5"
+# )
+
+# trajectory = pedpy.load_trajectory_from_ped_data_archive_hdf5(trajectory_file=file)
+
+# pedpy.plot_trajectories(traj=trajectory, walkable_area=walkable_area).set_aspect(
+#     "equal"
+# )
+# plt.show()
+##################################
+
+
+## all starting positions (for left to right when looking to the bottleneck)
+starting_positions = [
+    (-4, -0.5),
+    (-3.7, -2.15),
+    (-2.15, -3.7),
+    (0, -4),
+    (2.15, -3.7),
+    (3.7, -2.15),
+    (4, -0.5),
+]
+
+num_agents = len(starting_positions)  # one agent per starting position
+
+exit_area = Polygon([(-0.2, 1.2), (0.2, 1.2), (0.2, 1.3), (-0.2, 1.3)])
+
+
+## Setup Simulation
+trajectory_file = "single_indiv_btlnk_HumanoidModelV0.sqlite"  # output file
+simulation = jps.Simulation(
+    model=jps.HumanoidModelV0(),
+    geometry=area_exp,
+    trajectory_writer=jps.SqliteHumanoidTrajectoryWriter(
+        output_file=pathlib.Path(trajectory_file)
+    ),
+)
+
+exit_id = simulation.add_exit_stage(exit_area.exterior.coords[:-1])
+journey = jps.JourneyDescription([exit_id])
+journey_id = simulation.add_journey(journey)
+
+## Spawn agents
+v_distribution = normal(1.34, 0.5, num_agents)
+
+## Add the first agent with a fixed position
+simulation.add_agent(
+    jps.HumanoidModelV0AgentParameters(
+        journey_id=journey_id,
+        stage_id=exit_id,
+        position=starting_positions[0],
+        desiredSpeed=v_distribution[0],
+        height=1.75,
+    )
+)
+
+figure, ax = plt.subplots(figsize=(10, 6))
+pedpy.plot_walkable_area(walkable_area=pedpy.WalkableArea(area_exp), ax=ax).set_aspect(
+    "equal"
+)
+
+## run simulation
+# print(isinstance(agent.model, jps.py_jupedsim.HumanoidModelV0State))
+while simulation.agent_count() > 0 and simulation.iteration_count() < 3000:
+
+    try:
+        simulation.iterate()
+        # print(simulation.iteration_count())
+
+        ## plot the trajectory of the agents
+        for agent in simulation.agents():
+            ax.scatter(
+                agent.position[0],
+                agent.position[1],
+                color="blue",
+                s=10,
+                label="Agent Trajectory",
+            )
+        if simulation.iteration_count() % 1000 == 0:
+            plt.show()
+            figure, ax = plt.subplots(figsize=(10, 6))
+            pedpy.plot_walkable_area(
+                walkable_area=pedpy.WalkableArea(area_exp), ax=ax
+            ).set_aspect("equal")
+    except:
+        plt.show()
